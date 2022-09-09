75e01abb4d44d56811c46374380c22b3d5633a48
joebentley <joebentley10@gmail.com>
2022-09-09 11:16:19 +0200

    Changed stringifications to big switch statements w/ default assert trap

--- Token.cpp
+++ Token.cpp
@@ -2,56 +2,96 @@
 #include <JK/Format.h>
 #include <vector>
 
-const std::vector<std::string> token_names = {
-        "string",
-        "number",
-        "plus",
-        "minus",
-        "asterisk",
-        "slash",
-        "percent",
-        "chevron",
-        "angle_right",
-        "angle_left",
-        "angle_right_equals",
-        "angle_left_equals",
-        "equals_equals",
-        "bang_equals",
-        "bang",
-        "left_paren",
-        "right_paren",
-        "true",
-        "false"};
-
 std::string token_type_string(TokenType type)
 {
-    return token_names[static_cast<int>(type)];
+    switch (type) {
+        case TokenType::string:
+            return "string";
+        case TokenType::number:
+            return "number";
+        case TokenType::plus:
+            return "plus";
+        case TokenType::minus:
+            return "minus";
+        case TokenType::asterisk:
+            return "asterisk";
+        case TokenType::slash:
+            return "slash";
+        case TokenType::percent:
+            return "percent";
+        case TokenType::chevron:
+            return "chevron";
+        case TokenType::angle_right:
+            return "angle_right";
+        case TokenType::angle_left:
+            return "angle_left";
+        case TokenType::angle_right_equals:
+            return "angle_right_equals";
+        case TokenType::angle_left_equals:
+            return "angle_left_equals";
+        case TokenType::equals_equals:
+            return "equals_equals";
+        case TokenType::bang_equals:
+            return "bang_equals";
+        case TokenType::bang:
+            return "bang";
+        case TokenType::left_paren:
+            return "left_paren";
+        case TokenType::right_paren:
+            return "right_paren";
+        case TokenType::true_tok:
+            return "true_tok";
+        case TokenType::false_tok:
+            return "false_tok";
+        default:
+            assert(false);
+    }
 }
-
-const std::vector<std::string> token_lexemes = {
-        "string",
-        "number",
-        "+",
-        "-",
-        "*",
-        "/",
-        "%",
-        "^",
-        ">",
-        "<",
-        ">=",
-        "<=",
-        "==",
-        "!=",
-        "!",
-        "(",
-        ")",
-        "true",
-        "false"};
 
 std::string token_type_lexeme(TokenType type)
 {
-    return token_lexemes[static_cast<int>(type)];
+    switch (type) {
+        case TokenType::string:
+            return "string";
+        case TokenType::number:
+            return "number";
+        case TokenType::plus:
+            return "+";
+        case TokenType::minus:
+            return "-";
+        case TokenType::asterisk:
+            return "*";
+        case TokenType::slash:
+            return "/";
+        case TokenType::percent:
+            return "%";
+        case TokenType::chevron:
+            return "^";
+        case TokenType::angle_right:
+            return ">";
+        case TokenType::angle_left:
+            return "<";
+        case TokenType::angle_right_equals:
+            return ">=";
+        case TokenType::angle_left_equals:
+            return "<=";
+        case TokenType::equals_equals:
+            return "==";
+        case TokenType::bang_equals:
+            return "!=";
+        case TokenType::bang:
+            return "!";
+        case TokenType::left_paren:
+            return "(";
+        case TokenType::right_paren:
+            return ")";
+        case TokenType::true_tok:
+            return "true";
+        case TokenType::false_tok:
+            return "false";
+        default:
+            assert(false);
+    }
 }
 
 std::string Token::string() const
